674c0331f0ae1f898d87b31f83a0bd7fd0852c23
Basil <basiltalias92@gmail.com>
2017-12-03 13:13:56 -0500

    Added function example

---
+++ functions.py
@@ -0,0 +1,21 @@
+#This example is to use function for Adding, Substract, Multiply and Divide
+
+def add(a,b):
+    print(f"Adding {a} + {b}")
+    return a + b
+
+def substract(a,b):
+    print(f"Substract {a} - {b}")
+    return a - b
+
+def multiply (a,b):
+    print(f"Multiply {a} * {b}")
+    return a * b
+
+def divide(a,b):
+    print(f"Divide {a} / {b}")
+    return a / b
+
+print("Add",add(30,5))
+print("Subtract ",substract(30,5))
+
