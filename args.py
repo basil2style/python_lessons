38bf3a81c1e1ec17a15460cda5fe6220525090b8
Basil <basiltalias92@gmail.com>
2017-12-03 12:46:07 -0500

    Adde first lesson args

---
+++ args.py
@@ -0,0 +1,9 @@
+#This example is about how to read arguments on python
+from sys import argv
+
+script, first, second, third = argv
+print("This script is called:", script);
+print("First :",first);
+print("Second :", second);
+print("Third :", third);
+
